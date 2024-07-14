68600e28d4a09cd7c2638d0bff99d8df4249ea93
vyankateshkamod <vyankateshkamod@gmail.com>
2024-07-14 18:36:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,4 @@ app.get("/", function (req, res) {
     res.render("index");
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(3000)
